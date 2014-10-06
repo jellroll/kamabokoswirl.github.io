b493fc48429098437122f194619445e9d4f0d02b
Stephanie Hom <steph@5deadly.com>
2014-10-05 22:00:17 -0400

    Updated captions.

--- GA_3_Gallery/js/application.js
+++ GA_3_Gallery/js/application.js
@@ -1,6 +1,6 @@
 images = ["img/image-01.jpg", "img/image-02.jpg", "img/image-03.jpg", "img/image-04.jpg", "img/image-05.jpg",  "img/image-06.jpg", "img/image-07.jpg", "img/image-08.jpg", "img/image-09.jpg", "img/image-10.jpg", "img/image-11.jpg", "img/image-12.jpg"];
 
-captions = ["Wood Chips", "Mushroom", "Mushroom Friends", "Haleakala Rock", "Mossy", "Leaves", "Paper-like Bark", "Tree Bark", "Rainbow Eucalyptus", "More Trees", "Hanging Berries", "Wood On Black Sand Beach"];
+captions = ["Haleakala", "Mushroom", "Mushroom Friends", "Haleakala Rock", "Mossy", "Leaves", "Paper-like Bark", "Tree Bark", "Rainbow Eucalyptus", "More Trees", "Hanging Berries", "Wood On Black Sand Beach"];
 
 // Change image and caption
 var currentImage = 0;
